471e8dab341d7c1fdf11eb27609c34d08e903ca3
SoundaryaSC6 <153070841+SoundaryaSC6@users.noreply.github.com>
2025-10-26 22:14:37 +0530

    Fix comment formatting in index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
 );
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// unregister() to register() below. Note, this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
 
